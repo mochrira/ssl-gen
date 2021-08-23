c7d6fe04e074806c2cc2770feb681664f7391ea3
Moch. Rizal Rachmadani <mochrira@gmail.com>
2021-08-23 10:20:52 +0700

    adding script to generate ip

---
+++ ip.bat
@@ -0,0 +1,10 @@
+@ECHO off
+set OPENSSL_CONF=D:\sistem\apache\conf\openssl.cnf
+set openssl=D:\sistem\apache\bin\openssl.exe
+set certPath=D:\sistem\apache\ssl
+set extPath=D:\sistem\apache\ssl\ext
+set newLine=^& echo.
+%openssl% req -new -nodes -newkey rsa:2048 -keyout "%certPath%\%1.key" -out "%certPath%\%1.csr" -subj "/C=ID/ST=EastJava/L=Banyuwangi/O=%1-Cert/CN=%1"
+(echo authorityKeyIdentifier = keyid,issuer %newLine%keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment %newLine%subjectAltName = @alt_names %newLine%[alt_names] %newLine%IP.1 = %1 %newLine%IP.2 = %2) > "%extPath%\%1.ext"
+%openssl% x509 -req -sha256 -days 1024 -in "%certPath%\%1.csr" -CA "%certPath%\%3.pem" -CAkey "%certPath%\%3.key" -CAcreateserial -extfile "%extPath%\%1.ext" -out "%certPath%\%1.crt"
+PAUSE
